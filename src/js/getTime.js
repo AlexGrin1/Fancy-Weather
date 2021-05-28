5f1ed400c7f807afae978e68c76ba48de9a4bd5b
AlexGrin1 <a_grynevich@mail.ru>
2021-05-29 00:52:00 +0300

    refactor: fix comment

--- src/js/getTime.js
+++ src/js/getTime.js
@@ -11,16 +11,10 @@ function getDate() {
 
 function showTime() {
   const today = new Date();
-  const hour = today.getHours();
-  const min = today.getMinutes();
-  const sec = today.getSeconds();
-  time.innerHTML = `${hour}:${addZero(min)}:${addZero(sec)}`;
-  setTimeout(() => {
-    getDate();
-    showTime();
-  }, 1000);
+  const hour = today.getHours().toString();
+  const min = today.getMinutes().toString();
+  const sec = today.getSeconds().toString();
+  time.innerHTML = `${hour.padStart(2, "0")}:${min.padStart(2, "0")}:${sec.padStart(2, "0")}`;
 }
-function addZero(n) {
-  return (parseInt(n, 10) < 10 ? "0" : "") + n;
-}
-showTime();
+setInterval(getDate, 1000);
+setInterval(showTime, 1000);
--- src/js/weather.js
+++ src/js/weather.js
@@ -7,14 +7,7 @@ const innerInfo = document.querySelector(".innerInfo");
 const weatherOn3Days = document.querySelector(".weatherOn3Days");
 const buttonSearch = document.querySelector("button");
 const inputCity = document.querySelector("input");
-let city;
 let locationGeo;
-let i;
-
-function getCity() {
-  city = inputCity.value || userLocation();
-  getWeather(city);
-}
 
 async function getWeather(city) {
   try {
@@ -22,7 +15,6 @@ async function getWeather(city) {
       `https://api.weatherapi.com/v1/forecast.json?key=e656736c26754e098db140545212405&q=${city}&days=5`
     );
     const data = await response.json();
-    const i = await console.log(data);
     const locationGeo = [data.location.lon, data.location.lat];
     create(data);
     getMaps(locationGeo);
@@ -34,17 +26,13 @@ async function userLocation() {
   try {
     const response = await fetch("https://ipinfo.io?token=6520844a54f3ec");
     const resp = await response.json();
-    //const locationGeo = resp.loc.split(",");
-    const b = await console.log(resp);
     getWeather(resp.city);
-    //getMaps(locationGeo);
   } catch {
     alert("Что-то пошло не так");
   }
 }
 function getMaps(coordinates) {
-  mapboxgl.accessToken =
-    "pk.eyJ1IjoiamVyb21pdHJ1IiwiYSI6ImNrcDV0MXRmMjF4bDQyb213NGpxZTNiNDkifQ.VgwARiMKZjGIkaYakkpQQw";
+  mapboxgl.accessToken = "pk.eyJ1IjoiamVyb21pdHJ1IiwiYSI6ImNrcDV0MXRmMjF4bDQyb213NGpxZTNiNDkifQ.VgwARiMKZjGIkaYakkpQQw";
   var map = new mapboxgl.Map({
     container: "map",
     center: coordinates,
@@ -52,35 +40,22 @@ function getMaps(coordinates) {
     style: "mapbox://styles/mapbox/streets-v11",
   });
 }
-
-function create(link) {
-  const info = {
-    icon: link.current.condition.icon,
-    city: link.location.name,
-    country: link.location.country,
-    currentTemp_c: link.current.temp_c,
-    currentTemp_f: link.current.temp_f,
-    localTime: link.location.localTime,
-    wind_kph: link.current.wind_kph,
-    humidity: link.current.humidity,
-    feelslike_c: link.current.feelslike_c,
-    feelslike_f: link.current.feelslike_f,
-    text: link.current.condition.text,
-  };
-
-  location.textContent = `${link.location.name.toUpperCase()}, ${link.location.country.toUpperCase()}`;
-  temp.innerHTML = `${Math.round(link.current.temp_c)}&#176`;
+function createCurrentWeatherInfo(data) {
+  location.textContent = `${data.location.name.toUpperCase()}, ${data.location.country.toUpperCase()}`;
+  temp.innerHTML = `${Math.round(data.current.temp_c)}&#176`;
   const icon = document.createElement("img");
-  icon.setAttribute("src", link.current.condition.icon);
+  icon.setAttribute("src", data.current.condition.icon);
   icon.className = "icon";
   innerInfo.appendChild(icon);
+}
+function createFutureWeatherInfo(data) {
   const info_param = [
-    `${info.text}`,
-    `FEELS LIKE: ${Math.round(info.feelslike_c)} &#176`,
-    `WIND: ${Math.round(info.wind_kph * (5 / 18))} m/s`,
-    `HUMIDITY: ${info.humidity}%`,
+    `${data.current.condition.text}`,
+    `FEELS LIKE: ${Math.round(data.current.feelslike_c)} &#176`,
+    `WIND: ${Math.round(data.current.wind_kph * (5 / 18))} m/s`,
+    `HUMIDITY: ${data.current.humidity}%`,
   ];
-  link.forecast.forecastday.forEach((el) => {
+  data.forecast.forecastday.forEach((el) => {
     const blockDayWeather = document.createElement("div");
     blockDayWeather.className = "blockDayWeather";
     const weekDayNext = document.createElement("div");
@@ -111,6 +86,10 @@ function create(link) {
     innerInfo.appendChild(element);
   });
 }
+function create(data) {
+  createCurrentWeatherInfo(data);
+  createFutureWeatherInfo(data);
+}
 
 function clean() {
   location.innerHTML = "";
@@ -120,19 +99,40 @@ function clean() {
   on3days.forEach((el) => el.remove());
 }
 
+function debounce(fn, interval) {
+  let timer;
+  return function debounced() {
+    clearTimeout(timer);
+    let args = arguments;
+    let that = this;
+    timer = setTimeout(function callOriginalFn() {
+      fn.apply(that, args);
+    }, interval);
+  };
+}
+
+function onSearch() {
+  clean();
+  getWeather(inputCity.value);
+  inputCity.value = "";
+}
+
 document.addEventListener("DOMContentLoaded", (e) => {
-  buttonSearch.addEventListener("click", (ev) => {
-    clean();
-    //getCity();
-    getWeather(inputCity.value);
-  });
-  document.addEventListener("keydown", (event) => {
-    if (event.key === "Enter" && inputCity.value !== "undefined") {
-      clean();
-      //getCity();
-      getWeather(inputCity.value);
-      inputCity.value = "";
-    }
-  });
+  buttonSearch.addEventListener(
+    "click",
+    debounce((ev) => {
+      if (inputCity.value !== "") {
+        onSearch();
+      }
+    }, 150)
+  );
+  document.addEventListener(
+    "keydown",
+    debounce((event) => {
+      if (event.key === "Enter" && inputCity.value !== "") {
+        onSearch();
+      }
+    }, 150)
+  );
   userLocation();
 });
