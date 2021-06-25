2d4ebeb3a16ed59f4b166447f8e0585ab66d5d39
AlexGrin1 <a_grynevich@mail.ru>
2021-06-25 17:24:18 +0300

    fix all comment

--- src/js/app.js
+++ src/js/app.js
@@ -10,20 +10,17 @@ import {
   getRandomImage,
 } from './weather';
 import { showDateAndTime } from './timeUtils';
-import { lang, changeLanguage, currentLocation } from './languageUtil';
+import {
+  lang, changeLanguage, currentLocation, language,
+} from './languageUtil';
 
 document.addEventListener('DOMContentLoaded', () => {
   const refreshImage = document.getElementById('refreshImage');
-  const variantsLanguage = document.querySelectorAll('option');
   const form = document.querySelector('form');
+  const select = document.getElementById('choice_lang');
   getRandomImage();
   userLocation();
-  variantsLanguage.forEach((el) => {
-    el.removeAttribute('selected');
-    if (el.value === localStorage.getItem('language')) {
-      el.setAttribute('selected', '');
-    }
-  });
+  select.value = language;
   form.addEventListener('submit', (event) => {
     event.preventDefault();
     onSearch();
